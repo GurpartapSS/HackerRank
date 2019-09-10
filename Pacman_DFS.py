5100c15a6454c38ffefff9fc2f1a9fbb1398305d
GurpartapSS <gurpartapsingh.sarkaria@gmail.com>
2019-09-09 20:27:12 -0400

    Changes in variables

--- Pacman_DFS.py
+++ Pacman_DFS.py
@@ -1,12 +1,10 @@
-import queue
-
 pac_x, pac_y = list(map(int, input().split()))
 Dest_x, Dest_y = list(map(int, input().split()))
 n, m = list(map(int, input().split()))
 
 grid = []
 for i in range(n):
-	g.append(list(map(str, input())))
+    grid.append(list(map(str, input())))
 
 explore = []
 node_visited = []
@@ -15,32 +13,30 @@ final_route = None
 PossibleMoves = [[-1, 0], [0, -1], [0, 1], [1, 0]]
 
 explore.append([pac_x, pac_y, []])
-%empty list to append the routes
 
 while len(explore) > 0:
-	x, y, temp = explore.pop()
-	routes = list(temp)
-	routes.append([x, y])
-	node_visited.append([x, y])
+    x, y, temp = explore.pop()
+    routes = list(temp)
+    routes.append([x, y])
+    node_visited.append([x, y])
 
-	if x == Dest_x and y == Dest_y:
-	if final_route = None:
-		final_route = routes
-		break
+    if x == Dest_x and y == Dest_y:
+        if final_route == None:
+            final_route = routes
+            break
 
-	for move in PossibleMoves:
-		next_x, next_y = x + move[0], y + move[1]
-		if next_x < 0 or next_x >= m or next_y < 0 and next_y >= n:
-			continue
+    for move in PossibleMoves:
+        next_x, next_y = x + move[0], y + move[1]
+        if next_x < 0 or next_x >= m or next_y < 0 and next_y >= n:
+            continue
 
-		if grid[next_x][next_y] == '-' or grid[next_x][next_y] == '.':
-			grid[next_x][next_y] = '='
-			explore.append([next_x, next_y, routes])
+        if grid[next_x][next_y] == '-' or grid[next_x][next_y] == '.':
+            grid[next_x][next_y] = '='
+            explore.append([next_x, next_y, routes])
 print(str(len(node_visited)))
 for nodes in node_visited:
-	print(str(point[0]0 + " " +str(point[1]))
+    print(str(nodes[0]) + " " +str(nodes[1]))
 
 print(str(len(final_route)-1))
 for nodes in final_route:
-	print(str(point[0]0 + " " +str(point[1]))
-
+    print(str(nodes[0]) + " " +str(nodes[1]))
