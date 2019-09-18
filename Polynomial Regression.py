209ace4b94bdc164cf4f863bb7aec61d72c5a099
= <=>
2019-09-18 18:49:38 -0400

    updated poly reg

--- Polynomial Regression.py
+++ Polynomial Regression.py
@@ -1,16 +1,31 @@
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
 n_features, n_samples = list(map(int, input().split()))
 
 price = []
 feature = []
 
 for _ in range(n_samples):
-	prices = list(map(float, input().split()))[n_features]
-	features = list(map(float, input().split()))[:n_features]
-	price.append(prices)
-	f.append(features)
+	toll = list(map(float, input().split()))
+#	prices = list(map(float, input().split()))[n_features]
+#	features = list(map(float, input().split()))[:n_features]
+	price.append(toll[n_features])
+	feature.append(toll[:n_features])
 
 targets = int(input())
 target_feature = []
 
 for _ in range(targets):
 	targets = list(map(float, input().split()))[:n_features]
+
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(feature)
+pol_reg = LinearRegression()
+pol_reg.fit(X_poly, price)
+
+for i in targets:
+	assume = pol_reg.predict(poly_reg.fit_transform([targets[i]]))
+	print(assume)
