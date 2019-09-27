d426eea5f52bda167ad9a3bd85084b3831e90498
= <=>
2019-09-27 01:48:57 -0400

    .

--- Maze Puzzle.py
+++ Maze Puzzle.py
@@ -1,42 +1,18 @@
-pac_x, pac_y = list(map(int, input().split()))
-Dest_x, Dest_y = list(map(int, input().split()))
-n, m = list(map(int, input().split()))
-
-grid = []
-for i in range(n):
-    grid.append(list(map(str, input())))
-
-explore = []
-node_visited = []
-final_route = None
-
-PossibleMoves = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-SMove = ["UP","Down","LEFT","RIGHT"]
-explore.append([pac_x, pac_y, []])
-
-while len(explore) > 0:
-    x, y, temp = explore.pop()
-    routes = list(temp)
-    routes.append([x, y])
-    node_visited.append([x, y])
-
-    if x == Dest_x and y == Dest_y:
-        if final_route == None:
-            final_route = routes
+strs = ['UP','RIGHT','DOWN','LEFT']
+N = input()
+mp = []
+for i in range(3):
+    mp.append(input())
+#print mp    
+C = [mp[0][1], mp[1][2], mp[2][1], mp[1][0]]
+f=0
+for i in range(4):
+    if C[i]=='e':
+        print(strs[i])
+        f=1
+        break
+if f==0:
+    for i in range(4):
+        if C[i]=='-' or C[i]=='b':
+            print(strs[i])
             break
-
-    for move in PossibleMoves:
-        next_x, next_y = x + move[0], y + move[1]
-        if next_x < 0 or next_x >= m or next_y < 0 and next_y >= n:
-            continue
-
-        if grid[next_x][next_y] == '-' or grid[next_x][next_y] == '.':
-            grid[next_x][next_y] = '='
-            explore.append([next_x, next_y, routes])
-print(str(len(node_visited)))
-for nodes in node_visited:
-    print(str(nodes[0]) + " " +str(nodes[1]))
-
-print(str(len(final_route)-1))
-for nodes in final_route:
-    print(str(nodes[0]) + " " +str(nodes[1]))
