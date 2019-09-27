445329653e4325327f79d7106e5273ed6c1f3a78
Gurpartap_SS <31657994+GurpartapSS@users.noreply.github.com>
2019-09-27 01:47:35 -0400

    Update StockPrediction.py

--- StockPrediction.py
+++ StockPrediction.py
@@ -3,11 +3,11 @@ import math
 def printTransactions(m, k, d, name, owned, prices):
     slope_list=[]
     a = []
-    a.append(x)
-    for x in range(len(prices)):
+    for x in range(len(prices[0])):
+        a.append(x)
     a = np.array(a)
+    x=a
     for i in range(k):
-        x=a
         y= np.array(prices[i])
         A = np.vstack([x, np.ones(len(x))]).T
         slope, constat = np.linalg.lstsq(A, y)[0]
@@ -22,19 +22,19 @@ def printTransactions(m, k, d, name, owned, prices):
             #print prices[index_slope][-1]
             num_stock =math.floor(amount_remaining/prices[index_slope][-1])
             amount_remaining = amount_remaining - prices[index_slope][-1]*num_stock
-            trans[name[index_slope]] = names[index_slope]+' BUY ' + str(int(num_stock))
+            trans[name[index_slope]] = name[index_slope]+' BUY ' + str(int(num_stock))
             
         
     for j in range(k):
         index_slope = slope_list.index(sort_slope[j])
         if sort_slope[j]>0 and owned[index_slope]>0:
-            trans[name[index_slope]] = names[index_slope]+' SELL ' + str(owned[index_slope]) 
+            trans[name[index_slope]] = name[index_slope]+' SELL ' + str(owned[index_slope]) 
             
     print(len(trans.keys()))
     for x in trans:
         print(trans[x])
 
-m, k, d = list(map(int,input().strip().split()))
+m, k, d = input().split()
 m = float(m)
 k = int(k)
 d = int(d)
@@ -46,7 +46,7 @@ prices = []
 for _ in range(k):
     l = input().split()
     name.append(l[0])
-    owned.append(l[1])
+    owned.append(int(l[1]))
     prices.append([float(i) for i in l[2:]])
 
 printTransactions(m, k, d, name, owned, prices)
