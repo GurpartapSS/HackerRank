079e6bf73d29d6cab1407c06dfd955ec65587734
= <=>
2019-10-08 14:22:07 -0400

    added analysis for multiple classification methods

--- DocumentClassification.py
+++ DocumentClassification.py
@@ -17,29 +17,49 @@ x = [balls.strip() for balls in ada]
 x_token = [s.split(" ") for s in x]
 len(x) == len(y)
 
-#import nltk
-#from nltk.corpus import stopwords
-#stop_words = set(stopwords.words('english'));
-
 from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer()
-#X = vectorizer.fit_transform(x)
-from sklearn.feature_extraction.text import CountVectorizer
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(x)
-X_train_counts.shape
+vectorizer = TfidfVectorizer(sublinear_tf=True, stop_words='english')
+#vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(x)
+X.shape
 
-from sklearn.feature_extraction.text import TfidfTransformer
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-X_train_tfidf.shape
+#from sklearn.feature_extraction.text import CountVectorizer
+#count_vect = CountVectorizer()
+#X_train_counts = count_vect.fit_transform(x)
+#X_train_counts.shape
+#from sklearn.feature_extraction.text import TfidfTransformer
+#tfidf_transformer = TfidfTransformer()
+#X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+#X_train_tfidf.shape
+
 
 from sklearn.model_selection import train_test_split
+X_train,x_test,y_train,y_test=train_test_split(X,y,test_size = 0.1)
 from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB().fit(X_train_tfidf, y)
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 
-#from sklearn.pipeline import Pipeline
-#text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfVectorizer()),('clf', MultinomialNB())])
-test = vectorizer.fit_transform(X_train_tfidf[0])
-predicted = clf.predict(test)
->>>>>>> 9f361c6767a7aebdec4e8b6828907d6a32a373b4
+
+clfM = MultinomialNB().fit(X_train,y_train)
+#clfG = GaussianNB().fit(X_train,y_train)
+clfS = LinearSVC().fit(X_train,y_train)
+clfLR = LogisticRegression().fit(X_train,y_train)
+clfLC = LogisticRegressionCV().fit(X_train,y_train)
+
+from sklearn.metrics import accuracy_score
+predM = clfM.predict(x_test)
+predS = clfS.predict(x_test)
+predLR = clfLR.predict(x_test)
+predLC = clfLC.predict(x_test)
+
+accM = accuracy_score(predM,y_test)
+accS = accuracy_score(predS,y_test)
+accLR = accuracy_score(predLR,y_test)
+accLC = accuracy_score(predLC,y_test)
+print(accM,accS,accLR,accLC)
+
+t = ["my computer is not working"]
+T = vectorizer.transform(t)
+T.shape
+print(clfLC.predict(T))
